861532c5ca8302faf8491a12d2336258873038a0
Meng Chhiv <mengchhiv111@gmail.com>
2026-04-29 21:38:38 +0700

    Define route for Budget module to receive itinerary data

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,15 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SupabaseModule } from './supabase/supabase.module';
-import { ConfigModule } from '@nestjs/config';
+import { BudgetModule } from './budget/budget.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    SupabaseModule,
-  ],
+  imports: [SupabaseModule, BudgetModule],
   controllers: [AppController],
   providers: [AppService],
 })
