433dd5eec95f5f0e8acc772a4895fa08b26588da
Samnangalex06 <samnangalextt@gmail.com>
2026-05-12 19:36:31 +0700

    add desintaion and desination detail page

--- apps/client/src/modules/explore/services/destinationsApi.ts
+++ apps/client/src/modules/explore/services/destinationsApi.ts
@@ -10,6 +10,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:3000
  */
 export interface DestinationApiResponse {
   id: string
+  created_at?: string
   name: string
   description?: string
   province: string
